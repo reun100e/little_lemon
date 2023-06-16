85116b12c8c6b9f0271491bca6178d3082075e68
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2023-06-16 23:00:26 +0530

    Completed Homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import logo from "../images/Asset 20@4x.png";
 
 const Footer = () => {
-    return(
-        <>
-        
-        </>
-    );
-}
+  return (
+    <>
+      <div className="footer">
+        <img src={logo} alt="logo" />
+        <a href="/home">Homepage</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+      </div>
+      <h6 className="copyright">Copyright © 2023 Little Lemon. All rights reserved</h6>
+    </>
+  );
+};
 
 export default Footer;
