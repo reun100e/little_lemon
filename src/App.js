1fdb95094c89255835cd9449b6dab1b7758adb2f
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2023-06-19 20:05:08 +0530

    Updated component hierarchy

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import BookingForm from "./components/BookingForm"
+import Reserve from "./components/Reserve";
 import Homepage from "./components/Homepage";
 import CustomersSay from "./components/CustomersSay";
 
 function App() {
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="about" element={<About />} />
-        <Route path="booking" element={<BookingForm />} />
+        <Route path="booking" element={<Reserve />} />
         <Route path="testimonials" element={<CustomersSay />} />
       </Routes>
       <Footer />
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Hero from "./Hero";
 import Specials from "./Specials";
-import BookingForm from "./BookingForm";
+
 
 function Homepage() {
-  const [availableTimes, setAvailableTimes] = useState([
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
+
   return (
     <>
-      <BookingForm availableTimes={availableTimes} />
       <Hero />
       <Specials />
     </>
