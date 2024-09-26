31f1b2ffed3a7fe55b1ae7327ac8901274340686
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-09-26 13:24:17 +0530

    connected myaql

--- backend/littlelemon/littlelemon/settings.py
+++ backend/littlelemon/littlelemon/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "read_default_file": str(BASE_DIR / "db.cnf"),
+        },
     }
 }
 
