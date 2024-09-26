2376112fedd1c73390e160413546f4c87ec81eb1
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-09-26 14:15:09 +0530

    set up models.py

--- backend/littlelemon/restaurant/models.py
+++ backend/littlelemon/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# Menu model
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+# Booking model
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Booking by {self.name} for {self.no_of_guests} guests on {self.booking_date}"
